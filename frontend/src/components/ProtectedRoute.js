9d925a0580b7019cb8296ba43ca0245756c03606
RashmikaTR <tharusharashmika2001@gmail.com>
2026-05-12 23:08:28 +0530

    Created ProtectedRoute.js component

---
+++ frontend/src/components/ProtectedRoute.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { getToken } from "../services/auth";
+
+const ProtectedRoute = ({ children }) => {
+    const token = getToken();
+
+    if (!token) {
+        return <Navigate to="/" replace />;
+    }
+
+    return children;
+};
+
+export default ProtectedRoute;
