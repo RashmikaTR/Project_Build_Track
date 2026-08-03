9e0ceeb8cce8899ca36865cd0f2559dbd33c0bdf
RashmikaTR <tharusharashmika2001@gmail.com>
2026-08-04 01:23:32 +0530

    make changes of communication on App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,29 +5,31 @@ import Login from "./pages/Login";
 import LaborManagement from "./pages/LaborManagement";
 import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
+import Communication from "./pages/Communication";
 import InventoryManagement from "./pages/InventoryManagement";
 import SitesTasks from "./pages/SitesTasks";
 function App() {
   return (
     <LanguageProvider>
-    <Router>
-      <Routes>
-        {/* Public route (no Navbar) */}
-        <Route path="/" element={<Login />} />
+      <Router>
+        <Routes>
+          {/* Public route (no Navbar) */}
+          <Route path="/" element={<Login />} />
 
-        {/* Protected routes with Navbar */}
-        <Route
-        element={
+          {/* Protected routes with Navbar */}
+          <Route
+            element={
               <Layout />
-          }
+            }
           >
             <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/labor-management" element={<LaborManagement />} />
-          <Route path="/inventory" element={<InventoryManagement />} />
-          <Route path="/sites-tasks" element={<SitesTasks />} />
-        </Route>
-      </Routes>
-    </Router>
+            <Route path="/labor-management" element={<LaborManagement />} />
+            <Route path="/inventory" element={<InventoryManagement />} />
+            <Route path="/communication" element={<Communication />} />
+            <Route path="/sites-tasks" element={<SitesTasks />} />
+          </Route>
+        </Routes>
+      </Router>
     </LanguageProvider>
   );
 }
